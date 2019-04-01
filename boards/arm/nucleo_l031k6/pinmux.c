19df452b749a9599d589523a5ba43a67b7b87622
Giuliano Franchetto <giuliano.franchetto@intellinium.com>
2019-04-01 20:42:43 +0200

    Porting stm32l031-k6 dev board

---
+++ boards/arm/nucleo_l031k6/pinmux.c
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2018 INTELLINIUM <giuliano.franchetto@intellinium.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <kernel.h>
+#include <device.h>
+#include <init.h>
+#include <pinmux.h>
+#include <sys_io.h>
+
+#include <pinmux/stm32/pinmux_stm32.h>
+
+/* pin assignments for NUCLEO-L031K6 board */
+static const struct pin_config pinconf[] = {
+#ifdef CONFIG_UART_2
+	{STM32_PIN_PA9, STM32L0_PINMUX_FUNC_PA9_USART1_TX},
+	{STM32_PIN_PA10, STM32L0_PINMUX_FUNC_PA10_USART1_RX},
+#endif	/* CONFIG_UART_2 */
+#ifdef CONFIG_I2C_1
+	{STM32_PIN_PB7, STM32L0_PINMUX_FUNC_PB7_I2C1_SCL},
+	{STM32_PIN_PB6, STM32L0_PINMUX_FUNC_PB6_I2C1_SDA},
+#endif /* CONFIG_I2C_1 */
+};
+
+static int pinmux_stm32_init(struct device *port)
+{
+	ARG_UNUSED(port);
+
+	stm32_setup_pins(pinconf, ARRAY_SIZE(pinconf));
+
+	return 0;
+}
+
+SYS_INIT(pinmux_stm32_init, PRE_KERNEL_1,
+	 CONFIG_PINMUX_STM32_DEVICE_INITIALIZATION_PRIORITY);
